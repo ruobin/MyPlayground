2c0b4ce84d4853fd800a2a3cf9b24f549b46c986
Wang, Ruobin <Ruobin.Wang@disney.com>
2019-08-02 17:35:01 +0800

    add more problem solutions

---
+++ src/main/java/RemoveElementSolution.java
@@ -0,0 +1,29 @@
+
+/**
+ * Created by Ruobin Wang on 2019-08-02.
+ */
+public class RemoveElementSolution {
+
+    public int removeElement(int[] nums, int val) {
+
+        int i = 0;
+        int j = nums.length - 1;
+
+        while (i<=j) {
+            if(nums[i]==val) {
+                if(nums[j]==val) {
+                    j--;
+                } else {
+                    //swap
+                    int temp = nums[i];
+                    nums[i]=nums[j];
+                    nums[j] = temp;
+                    i++;
+                    j--;
+                }
+            } else i++;
+        }
+
+        return i;
+    }
+}
