043a25ae75ee8361614cfc0759f14497f6fc211a
ruobin.wang <ruobin.wang.us@gmail.com>
2019-12-09 23:18:40 +0800

    refine solution and add git ignore file

--- src/main/java/TwoSimilarWordsSolution.java
+++ src/main/java/TwoSimilarWordsSolution.java
@@ -14,11 +14,17 @@ public class TwoSimilarWordsSolution {
             set.add(input.charAt(i));
         }
 
+        boolean[] sourceCharactorMapArray = new boolean[26];
+        for (int i = 0; i < input.length(); i++) {
+            int ascii = (int) input.charAt(i);
+            sourceCharactorMapArray[ascii - 97] = true;
+        }
+
         int count = 0;
         String wordsList = br.readLine();
         String[] wordsArray = wordsList.split(" ");
         for (int i = 0; i < wordsArray.length; i++) {
-            if (isTwoWordsSimilar(set, wordsArray[i])) {
+            if (isTwoWordsSimilarUsingBooleanArray(sourceCharactorMapArray, wordsArray[i])) {
                 count++;
             }
         }
@@ -34,6 +40,26 @@ public class TwoSimilarWordsSolution {
             set.add(word.charAt(i));
         }
         return characterSet.equals(set);
+    }
+
+    private static boolean isTwoWordsSimilarUsingBooleanArray(boolean[] characterMapArray, String target) {
+
+        if (target == null || target.isEmpty()) return false;
+
+        boolean[] targetCharacterMapArray = new boolean[26];
+        for (int i = 0; i < target.length(); i++) {
+            int ascii = (int) target.charAt(i);
+            targetCharacterMapArray[ascii - 97] = true;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (characterMapArray[i] == targetCharacterMapArray[i]) {
+                continue;
+            } else {
+                return false;
+            }
+        }
+        return true;
     }
 
 }
